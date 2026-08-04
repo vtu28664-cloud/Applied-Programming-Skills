9b15d872ad0da63e06c259ac17017e2f0bca8c5c
vtu28664-cloud <vtu28664@veltech.edu.in>
2026-08-04 14:33:47 +0530

    Time: 2 ms (41.94%), Space: 94.4 MB (52.17%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+    int buy_price = prices[0];
+    int profit = 0;
+    for(int i = 1; i < prices.length; i++) {
+        if(prices[i] < buy_price) {
+            buy_price = prices[i];
+        } else {
+            int current_profit = prices[i] - buy_price;
+            profit = Math.max(current_profit, profit);
+        }
+    }    
+        return profit;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(s.maxProfit(prices));
+    }
+}
