3d89dc721d4f75d074227e3ba0fdefe1bfe8f3c7
vtu28664-cloud <vtu28664@veltech.edu.in>
2026-09-08 15:30:06 +0530

    Time: 2 ms (88.39%), Space: 46.4 MB (25.65%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Stack<Integer>st = new Stack<>();
-        int j = 0;
-        for(int x : pushed) {
-            st.push(x);
-            while(!st.isEmpty() && j < popped.length && st.peek() == popped[j]) {
-                st.pop();
-                j++;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int i = 0;
+        for(int val : pushed) {
+            stack.push(val);
+            while(!stack.isEmpty() && stack.peek() == popped[i]) {
+                stack.pop();
+                i++;
             }
         }
-        return st.isEmpty();
-        
+        return i == popped.length;
     }
 }
